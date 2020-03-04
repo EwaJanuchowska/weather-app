d8378ed3611d813f87d0b9031dd0876c1a903422
Ewa Januchowska <tournesol.ewa@gmail.com>
2020-03-04 22:02:10 +0100

    Create base layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
 import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Grid,
+  Paper,
+  Container
+} from "@material-ui/core";
 
-const App = () => <div>Weather App</div>;
+const App = () => (
+  <>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h5">Weather app</Typography>
+      </Toolbar>
+    </AppBar>
+
+    <main className="main-content center">
+      <Container>
+        <Grid container>
+          <Grid item xs={12}>
+            <Paper square className="paper">
+              
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
+    </main>
+
+    <footer className="main-footer">
+      &copy; 2020 Weather App
+    </footer>
+  </>
+);
 
 export default App;
