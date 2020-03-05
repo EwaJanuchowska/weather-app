d1949e9556a549221841cc3713a3ca8dc1c67ce9
Ewa Januchowska <tournesol.ewa@gmail.com>
2020-03-05 22:09:05 +0100

    Fix footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Grid, Paper, Container, Box } from "@material-ui/core";
 import WeatherSymbol from "./components/WeatherSymbol";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import MainContent from "./components/MainContent";
 
 const App = () => (
@@ -21,24 +22,26 @@ const App = () => (
               Search for your city/region to check the weather
             </Box>
             <Box>
-            <WeatherSymbol type="hot" />
-            <WeatherSymbol type="clear" />
-            <WeatherSymbol type="partlyCloudy" />
-            <WeatherSymbol type="windy" />
-            <WeatherSymbol type="snow" />
-            <WeatherSymbol type="cloudy" />
-            <WeatherSymbol type="scatteredSnow" />
-            <WeatherSymbol type="thunderstorms" />
-            <WeatherSymbol type="thunderstormsWithRain" />
-            <WeatherSymbol type="showers" />
-            <WeatherSymbol type="drizzle" />
-            <WeatherSymbol type="rainWithSun" />
+              <WeatherSymbol type="hot" />
+              <WeatherSymbol type="clear" />
+              <WeatherSymbol type="partlyCloudy" />
+              <WeatherSymbol type="windy" />
+              <WeatherSymbol type="snow" />
+              <WeatherSymbol type="cloudy" />
+              <WeatherSymbol type="scatteredSnow" />
+              <WeatherSymbol type="thunderstorms" />
+              <WeatherSymbol type="thunderstormsWithRain" />
+              <WeatherSymbol type="showers" />
+              <WeatherSymbol type="drizzle" />
+              <WeatherSymbol type="rainWithSun" />
             </Box>
             <Paper square className="paper"></Paper>
           </Grid>
         </Grid>
       </Container>
     </MainContent>
+
+    <Footer />
   </>
 );
 
